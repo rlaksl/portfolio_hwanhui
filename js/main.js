7c99fc37412f55bc743f382afa757d02e14dd55e
rlaksl <eehiuh@gmail.com>
2025-08-26 15:02:17 +0900

    포트폴리오 코드 리팩토링 및 반응형 개선

--- js/main.js
+++ js/main.js
@@ -1,81 +1,102 @@
-// html 문서를 다 읽고나서 script 읽으라고 주문해야 함
-// 오프닝 구문 $(document).ready(function(){}) 쓰고 시작해야 함
-// 오프닝 구문 축약형 $(function(){});
-
-$(document).ready(function(){
+// 문서가 준비되면 실행
+$(document).ready(function() {
   
-//스크롤했을 때 가로로
-  let slideCount = $('figure').length;
-  let slideWidth = $('figure').width() + 20;
-
-  let totalWidth = slideWidth * slideCount + 800;
-
-  $('section').width(totalWidth);
-  $('body').height(totalWidth);
-
-  $(window).on('scroll', function(){
-    let scr = $(this).scrollTop();
-    $('section').stop().animate({
-      left : -scr
-    })
-  });
-
-  $('.navi li').on('click', function(){
-    let i = $(this).index();
-    $('html, body').stop().animate({
-      scrollTop : slideWidth * 5 * i});
-    $(this).addClass('on').siblings().removeClass('on');
-  })
-
-  $('figure').on('click', function(){
-    $(this).addClass('on').siblings().removeClass('on');
-    // $(this).css({backgroundImage : 'url(img/img.jpg)'})
-    let _img = $(this).find('img').attr('src');
-    $(this).css({backgroundImage : `url(${_img})`})
-    .siblings().css({backgroundImage : ``});
-
-    let winWidth = $(window).width();
-    let pos = winWidth / 2 - 390;
-    let i = $(this).index();
-    $('html, body').scrollTop(slideWidth*i-pos);
-
-    // 선택자의 .scrollTop('값') 값을 입력하면 그 위치로 감
-    // 비워두면 선택자의 수직스크롤 위치 구하기
+  // 네비게이션 클릭 이벤트 (기존 코드 정리)
+  function initNavigation() {
+    $('nav ul li').on('click', function() {
+      $(this).addClass('on').siblings().removeClass('on');
+    });
+  }
+  
+  // 부드러운 스크롤 기능 추가
+  function initSmoothScroll() {
+    $('a[href^="#"]').on('click', function(e) {
+      e.preventDefault();
+      
+      const target = this.hash;
+      const $targetElement = $(target);
+      
+      if ($targetElement.length) {
+        $('html, body').animate({
+          scrollTop: $targetElement.offset().top
+        }, 800);
+      }
+    });
+  }
+  
+  // 스크롤 위치에 따른 네비게이션 하이라이트
+  function initScrollSpy() {
+    $(window).on('scroll', function() {
+      const scrollPosition = $(window).scrollTop() + 100;
+      const sections = ['profileWrap', 'javaNyangWrap', 'hdexWrap'];
+      
+      sections.forEach(function(sectionId, index) {
+        const $section = $('#' + sectionId);
+        if ($section.length) {
+          const sectionTop = $section.offset().top;
+          const sectionBottom = sectionTop + $section.outerHeight();
+          
+          if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+            $('nav ul li').removeClass('on');
+            $('nav ul li').eq(index + 1).addClass('on');
+          }
+        }
+      });
+    });
+  }
+  
+  // 외부 링크 안전하게 열기
+  function initExternalLinks() {
+    $('a[target="_blank"]').on('click', function(e) {
+      // 기본 동작을 막지 않고 그대로 실행
+      console.log('외부 링크 열기: ' + $(this).attr('href'));
+    });
+  }
+  
+  // 버튼 호버 효과 개선
+  function initButtonEffects() {
+    $('.button li a').on('mouseenter', function() {
+      $(this).stop().animate({
+        backgroundColor: '#414141',
+        color: '#f2f2f2'
+      }, 200);
+    }).on('mouseleave', function() {
+      $(this).stop().animate({
+        backgroundColor: 'transparent',
+        color: '#414141'
+      }, 200);
+    });
+  }
+  
+  // 이미지 로딩 에러 처리
+  function initImageErrorHandling() {
+    $('img').on('error', function() {
+      console.log('이미지 로딩 실패: ' + $(this).attr('src'));
+      // 대체 이미지나 처리 로직을 여기에 추가할 수 있습니다.
+    });
+  }
+  
+  // 페이지 로딩 완료 후 페이드인 효과
+  function initPageAnimation() {
+    $('body').css('opacity', '0').animate({
+      opacity: 1
+    }, 1000);
+  }
+  
+  // 모든 초기화 함수 실행
+  function init() {
+    initNavigation();
+    initSmoothScroll();
+    initScrollSpy();
+    initExternalLinks();
+    initButtonEffects();
+    initImageErrorHandling();
+    initPageAnimation();
     
-  });
-
-  $('button').on('click', function(e){
-    e.stopPropagation(); // 버튼을 클릭해도 figure 안에 있기 때문에  부모태그(상위요소)인 figure에 removeClass가 전파되지 않게 막아주는 기능
-    $(this).parent('figure').removeClass('on');
-  });
+    console.log('포트폴리오 스크립트 초기화 완료');
+  }
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//오프닝구문
+  // 초기화 실행
+  init();
+  
 });
